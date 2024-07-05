47925ca9aec20f65f25194b2055e4d85b359eef8
Pierre-Yves <pierre-yves.monnet@laposte.net>
2024-07-05 11:38:24 -0700

    First version

---
+++ src/Example.cs
@@ -0,0 +1,114 @@
+sing System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog.Extensions.Logging;
+using Zeebe.Client;
+using Zeebe.Client.Api.Responses;
+using Zeebe.Client.Api.Worker;
+
+namespace Client.Examples
+{
+    internal class Program
+    {
+        private static readonly string DemoProcessPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+        "Resources", "demo-net-process.bpmn");
+        private static readonly string ZeebeUrl = "0.0.0.0:26500";
+        private static readonly string ProcessInstanceVariables = "{\"a\":\"123\"}";
+        private static readonly string JobType = "net-worker";
+        private static readonly string WorkerName = Environment.MachineName;
+        private static readonly long WorkCount = 100L;
+
+        public static async Task Main(string[] args)
+        {
+            // create zeebe client
+            var client = ZeebeClient.Builder()
+                .UseLoggerFactory(new NLogLoggerFactory())
+                .UseGatewayAddress(ZeebeUrl)
+                .UsePlainText()
+                .Build();
+
+            var topology = await client.TopologyRequest()
+                .Send();
+            Console.WriteLine(topology);
+            /*
+            await client.NewPublishMessageCommand()
+                .MessageName("csharp")
+                .CorrelationKey("wow")
+                .Variables("{\"realValue\":2}")
+                .Send();
+*/
+            // deploy
+            /*
+            var deployResponse = await client.NewDeployCommand()
+                .AddResourceFile(DemoProcessPath)
+                .Send();
+*/
+            // create process instance
+            // var processDefinitionKey = deployResponse.Processes[0].ProcessDefinitionKey;
+            var processDefinitionKey = "demo-net-process";
+
+            var processInstance = await client
+                .NewCreateProcessInstanceCommand()
+                .ProcessDefinitionKey(processDefinitionKey)
+                .Variables(ProcessInstanceVariables)
+                .Send();
+
+            await client.NewSetVariablesCommand(processInstance.ProcessInstanceKey).Variables("{\"wow\":\"this\"}").Local().Send();
+
+            for (var i = 0; i < WorkCount; i++)
+            {
+                await client
+                    .NewCreateProcessInstanceCommand()
+                    .ProcessDefinitionKey(processDefinitionKey)
+                    .Variables(ProcessInstanceVariables)
+                    .Send();
+            }
+
+            // open job worker
+            using (var signal = new EventWaitHandle(false, EventResetMode.AutoReset))
+            {
+                client.NewWorker()
+                      .JobType(JobType)
+                      .Handler(HandleJob)
+                      .MaxJobsActive(5)
+                      .Name(WorkerName)
+                      .AutoCompletion()
+                      .PollInterval(TimeSpan.FromSeconds(1))
+                      .Timeout(TimeSpan.FromSeconds(10))
+                      .Open();
+
+                // blocks main thread, so that worker can run
+                signal.WaitOne();
+            }
+        }
+
+        private static void HandleJob(IJobClient jobClient, IJob job)
+        {
+            // business logic
+            var jobKey = job.Key;
+            Console.WriteLine("Handling job: " + job);
+
+            if (jobKey % 3 == 0)
+            {
+                jobClient.NewCompleteJobCommand(jobKey)
+                    .Variables("{\"foo\":2}")
+                    .Send()
+                    .GetAwaiter()
+                    .GetResult();
+            }
+            else if (jobKey % 2 == 0)
+            {
+                jobClient.NewFailCommand(jobKey)
+                    .Retries(job.Retries - 1)
+                    .ErrorMessage("Example fail")
+                    .Send()
+                    .GetAwaiter()
+                    .GetResult();
+            }
+            else
+            {
+                // auto completion
+            }
+        }
+    }
